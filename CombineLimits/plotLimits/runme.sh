98ba7546a9ca799534c75677d1b2ad84aa4d10be
alpana-hep <km.alpana@cern.ch>
2024-07-13 20:21:41 +0530

    adding plotting scripts

---
+++ CombineLimits/plotLimits/runme.sh
@@ -0,0 +1,12 @@
+root -b -q 'overlay_multifile.C("Results","T5bbbbZg",1)'
+root -b -q 'overlay_multifile.C("Results","T5bbbbZg",2)'
+root -b -q 'overlay_multifile.C("Results","T5bbbbZg",3)'
+root -b -q 'overlay_multifile.C("Results","T5bbbbZg",4)'
+root -b -q 'overlay_multifile.C("Results","T5bbbbZg",5)'
+root -b -q 'overlay_multifile.C("Results","T5bbbbZg",6)'
+root -b -q 'overlay_multifile.C("Results","T5bbbbZg",7)'
+
+
+
+
+
---
+++ updateOptimizationStudies/PlottingScripts/working.sh
@@ -0,0 +1,202 @@
+# # ## plots for different modesl -
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5bbbbZg","T5bbbbZg","2500")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5bbbbZg","T5bbbbZg","2400")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5bbbbZg","T5bbbbZg","2300")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5bbbbZg","T5bbbbZg","2200")'
+
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5qqqqHg","T5qqqqHg","2500")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5qqqqHg","T5qqqqHg","2400")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5qqqqHg","T5qqqqHg","2300")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5qqqqHg","T5qqqqHg","2200")'
+
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5ttttZg","T5ttttZg","2500")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5ttttZg","T5ttttZg","2600")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5ttttZg","T5ttttZg","2400")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5ttttZg","T5ttttZg","2300")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5ttttZg","T5ttttZg","2200")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T5ttttZg","T5ttttZg","2100")'
+
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T6ttZg","T6ttZg","1300")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T6ttZg","T6ttZg","1400")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T6ttZg","T6ttZg","1600")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/T6ttZg","T6ttZg","1800")'
+
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/TChiWG","TChiWG","0")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Signals/TChiNG","TChiNG","0")'
+
+# # # background
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Backgrounds/WGJets","WGJets","0")'
+# root -b	-q 'overlay_1file.C("Results/withBaselineSelection/Backgrounds/WJets","WlnuJets","0")'
+# root -b	-q 'overlay_1file.C("Results/withBaselineSelection/Backgrounds/TTGJets","ttbarG","0")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Backgrounds/TTJets","ttbarJets","0")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Backgrounds/GJets","GJets","0")'
+# root -b -q 'overlay_1file.C("Results/withBaselineSelection/Backgrounds/ZNuNuGJets","ZnunuGJets","0")'
+# ## overlay Plots
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5bbbbZg","T5bbbbZg","2500")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5bbbbZg","T5bbbbZg","2400")'
+
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5bbbbZg","T5bbbbZg","2300")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5bbbbZg","T5bbbbZg","2200")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5qqqqHg","T5qqqqHg","2500")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5qqqqHg","T5qqqqHg","2400")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5qqqqHg","T5qqqqHg","2300")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5qqqqHg","T5qqqqHg","2200")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5ttttZg","T5ttttZg","2500")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5ttttZg","T5ttttZg","2400")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5ttttZg","T5ttttZg","2300")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T5ttttZg","T5ttttZg","2200")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T6ttZg","T6ttZg","1300")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T6ttZg","T6ttZg","1400")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T6ttZg","T6ttZg","1600")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/T6ttZg","T6ttZg","1800")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/TChiWG","TChiWG","0")'
+# root -b -q 'overlay_multifile.C("Results/withBaselineSelection/Signals/OverlayPlots/TChiNG","TChiNG","0")'
+
+# ## stacked plots
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5bbbbZg","T5bbbbZg","2300")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5bbbbZg","T5bbbbZg","2400")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5bbbbZg","T5bbbbZg","2500")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5bbbbZg","T5bbbbZg","2200")'
+
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5qqqqHg","T5qqqqHg","2500")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5qqqqHg","T5qqqqHg","2400")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5qqqqHg","T5qqqqHg","2300")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5qqqqHg","T5qqqqHg","2200")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5ttttZg","T5ttttZg","2500")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5ttttZg","T5ttttZg","2400")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5ttttZg","T5ttttZg","2300")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T5ttttZg","T5ttttZg","2200")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T6ttZg","T6ttZg","1300")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T6ttZg","T6ttZg","1400")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T6ttZg","T6ttZg","1600")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/T6ttZg","T6ttZg","1800")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/TChiWG","TChiWG","0")'
+root -b -q 'StackPlots_multifile.C("Results/BDTwithT5bbbbZg_7variables/TChiNG","TChiNG","0")'
+
+
+
+# ### 2D plots
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5bbbbZg","T5bbbbZg","2500","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5bbbbZg","T5bbbbZg","2400","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5bbbbZg","T5bbbbZg","2300","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5bbbbZg","T5bbbbZg","2200","overlap_removal")'
+                                         
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5bbbbZg","T5bbbbZg","2500","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5bbbbZg","T5bbbbZg","2400","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5bbbbZg","T5bbbbZg","2300","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5bbbbZg","T5bbbbZg","2200","MET_200")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5bbbbZg","T5bbbbZg","2500","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5bbbbZg","T5bbbbZg","2400","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5bbbbZg","T5bbbbZg","2300","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5bbbbZg","T5bbbbZg","2200","MET_300")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5bbbbZg","T5bbbbZg","2500","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5bbbbZg","T5bbbbZg","2400","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5bbbbZg","T5bbbbZg","2300","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5bbbbZg","T5bbbbZg","2200","MET_300_phopt_100")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5bbbbZg","T5bbbbZg","2500","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5bbbbZg","T5bbbbZg","2400","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5bbbbZg","T5bbbbZg","2300","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5bbbbZg","T5bbbbZg","2200","MET_200_phopt_100")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5qqqqHg","T5qqqqHg","2500","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5qqqqHg","T5qqqqHg","2400","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5qqqqHg","T5qqqqHg","2300","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5qqqqHg","T5qqqqHg","2200","overlap_removal")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5qqqqHg","T5qqqqHg","2500","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5qqqqHg","T5qqqqHg","2400","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5qqqqHg","T5qqqqHg","2300","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5qqqqHg","T5qqqqHg","2200","MET_200")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5qqqqHg","T5qqqqHg","2500","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5qqqqHg","T5qqqqHg","2400","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5qqqqHg","T5qqqqHg","2300","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5qqqqHg","T5qqqqHg","2200","MET_300")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5qqqqHg","T5qqqqHg","2500","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5qqqqHg","T5qqqqHg","2400","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5qqqqHg","T5qqqqHg","2300","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5qqqqHg","T5qqqqHg","2200","MET_300_phopt_100")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5qqqqHg","T5qqqqHg","2500","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5qqqqHg","T5qqqqHg","2400","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5qqqqHg","T5qqqqHg","2300","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5qqqqHg","T5qqqqHg","2200","MET_200_phopt_100")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5ttttZg","T5ttttZg","2600","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5ttttZg","T5ttttZg","2500","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5ttttZg","T5ttttZg","2400","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5ttttZg","T5ttttZg","2300","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5ttttZg","T5ttttZg","2200","overlap_removal")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T5ttttZg","T5ttttZg","2100","overlap_removal")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5ttttZg","T5ttttZg","2600","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5ttttZg","T5ttttZg","2500","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5ttttZg","T5ttttZg","2400","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5ttttZg","T5ttttZg","2300","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5ttttZg","T5ttttZg","2200","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T5ttttZg","T5ttttZg","2100","MET_200")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5ttttZg","T5ttttZg","2600","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5ttttZg","T5ttttZg","2500","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5ttttZg","T5ttttZg","2400","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5ttttZg","T5ttttZg","2300","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5ttttZg","T5ttttZg","2200","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T5ttttZg","T5ttttZg","2100","MET_300")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5ttttZg","T5ttttZg","2600","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5ttttZg","T5ttttZg","2500","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5ttttZg","T5ttttZg","2400","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5ttttZg","T5ttttZg","2300","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5ttttZg","T5ttttZg","2200","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T5ttttZg","T5ttttZg","2100","MET_200_phopt_100")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5ttttZg","T5ttttZg","2600","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5ttttZg","T5ttttZg","2500","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5ttttZg","T5ttttZg","2400","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5ttttZg","T5ttttZg","2300","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5ttttZg","T5ttttZg","2200","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T5ttttZg","T5ttttZg","2100","MET_300_phopt_100")'
+
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T6ttZg","T6ttZg","1300","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T6ttZg","T6ttZg","1300","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T6ttZg","T6ttZg","1300","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T6ttZg","T6ttZg","1300","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T6ttZg","T6ttZg","1300","overlap_removal")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T6ttZg","T6ttZg","1400","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T6ttZg","T6ttZg","1400","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T6ttZg","T6ttZg","1400","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T6ttZg","T6ttZg","1400","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T6ttZg","T6ttZg","1400","overlap_removal")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T6ttZg","T6ttZg","1600","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T6ttZg","T6ttZg","1600","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T6ttZg","T6ttZg","1600","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T6ttZg","T6ttZg","1600","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T6ttZg","T6ttZg","1600","overlap_removal")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/T6ttZg","T6ttZg","1800","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/T6ttZg","T6ttZg","1800","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/T6ttZg","T6ttZg","1800","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/T6ttZg","T6ttZg","1800","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/T6ttZg","T6ttZg","1800","overlap_removal")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/TChiWG","TChiWG","0","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/TChiWG","TChiWG","0","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/TChiWG","TChiWG","0","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/TChiWG","TChiWG","0","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/TChiWG","TChiWG","0","overlap_removal")'
+
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt100/TChiNG","TChiNG","0","MET_300_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt100/TChiNG","TChiNG","0","MET_200_phopt_100")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_300_phopt40/TChiNG","TChiNG","0","MET_300")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_200_phopt40/TChiNG","TChiNG","0","MET_200")'
+# root -b -q 'two2D_kinematic.C("Results/2Dplots/WithMET_100_phopt40/TChiNG","TChiNG","0","overlap_removal")'
+
+
+
